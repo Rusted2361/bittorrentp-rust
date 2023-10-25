a36afc3c2fa634786450c5a3514226558f396a07
Hamza <hamzasajid4040@gmail.com>
2023-10-25 10:34:55 +0500

    pass 2nd stage panic handeld as bonus

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,27 @@
-use serde_json;
+use serde_json::{json, Value};
 use std::env;
 
 // Available if you need it!
 // use serde_bencode
-
 #[allow(dead_code)]
-fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
-    // If encoded_value starts with a digit, it's a number
-    if encoded_value.chars().next().unwrap().is_digit(10) {
+fn decode_bencoded_value(encoded_value: &str) -> Result<Value, String> {
+    // If encoded_value starts with a digit, it's a string
+    let first_char = encoded_value.chars().next().ok_or("Empty input")?; 
+    if first_char.is_digit(10) {
         // Example: "5:hello" -> "hello"
-        let colon_index = encoded_value.find(':').unwrap();
+        let colon_index = encoded_value.find(':').ok_or("Invalid format")?;
         let number_string = &encoded_value[..colon_index];
-        let number = number_string.parse::<i64>().unwrap();
+        let number = number_string.parse::<i64>().map_err(|e| format!("Invalid number format: {}", e))?;
         let string = &encoded_value[colon_index + 1..colon_index + 1 + number as usize];
-        return serde_json::Value::String(string.to_string());
-    } else {
-        panic!("Unhandled encoded value: {}", encoded_value)
+        Ok(Value::String(string.to_string()))
+    } else if first_char == 'i' {
+        // Example: i52e -> 52
+        let end_index = encoded_value.len() - 1;
+        let number = &encoded_value[1..end_index].parse::<i64>().unwrap();
+        Ok(json!(number))
+    }
+    else {
+        Err("Unhandled encoded value".to_string())
     }
 }
 
@@ -25,14 +31,13 @@ fn main() {
     let command = &args[1];
 
     if command == "decode" {
-        // You can use print statements as follows for debugging, they'll be visible when running tests.
-        //println!("Logs from your program will appear here!");
-
-        //Uncomment this block to pass the first stage
         let encoded_value = &args[2];
-        let decoded_value = decode_bencoded_value(encoded_value);
-        println!("{}", decoded_value.to_string());
-    } else {
+        match decode_bencoded_value(encoded_value) {
+            Ok(decoded_value) => println!("{}", decoded_value.to_string()),
+            Err(err) => eprintln!("Error: {}", err),
+        }
+    } 
+    else {
         println!("unknown command: {}", args[1])
     }
 }
